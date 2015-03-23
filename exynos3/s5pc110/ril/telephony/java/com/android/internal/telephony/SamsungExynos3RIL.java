64640d841ab59ee6e5d5fb251fc159be07d9de87
Dheeraj CVR <cvr.dheeraj@gmail.com>
2015-03-23 09:56:50 +0400

    exynos3: ril: fix compile for Lollipop
    Change-Id: I6c6b7bf68ac4b098e1d4f65a125589cf7b557ad2

--- exynos3/s5pc110/ril/telephony/java/com/android/internal/telephony/SamsungExynos3RIL.java
+++ exynos3/s5pc110/ril/telephony/java/com/android/internal/telephony/SamsungExynos3RIL.java
@@ -38,6 +38,7 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.SignalStrength;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import static com.android.internal.telephony.RILConstants.*;
 
 import com.android.internal.telephony.CallForwardInfo;
@@ -64,7 +65,7 @@ public class SamsungExynos3RIL extends RIL implements CommandsInterface {
     private boolean mIsSamsungCdma = SystemProperties.getBoolean("ro.ril.samsung_cdma", false);
     private Object mCatProCmdBuffer;
 
-    public SamsungExynos3RIL(Context context, int networkMode, int cdmaSubscription) {
+    public SamsungExynos3RIL(Context context, int networkMode, int cdmaSubscription, Integer instanceId) {
         super(context, networkMode, cdmaSubscription);
     }
 
@@ -921,16 +922,6 @@ public class SamsungExynos3RIL extends RIL implements CommandsInterface {
      * {@inheritDoc}
      */
     @Override
-    public void setCurrentPreferredNetworkType() {
-        if (RILJ_LOGD) riljLog("setCurrentPreferredNetworkType IGNORED");
-        /* Google added this as a fix for crespo loosing network type after
-         * taking an OTA. This messes up the data connection state for us
-         * due to the way we handle network type change (disable data
-         * then change then re-enable).
-         */
-    }
-
-    @Override
     public void setPreferredNetworkType(int networkType , Message response) {
         /* Samsung modem implementation does bad things when a datacall is running
          * while switching the preferred networktype.
@@ -1006,11 +997,11 @@ public class SamsungExynos3RIL extends RIL implements CommandsInterface {
             Rlog.d(RILJ_LOG_TAG, "Mobile Dataconnection is online setting it down");
             mDesiredNetworkType = networkType;
             mNetworktypeResponse = response;
-            ConnectivityManager cm =
-                (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            TelephonyManager ts =
+                (TelephonyManager)mContext.getSystemService(Context.TELEPHONY_SERVICE);
             //start listening for the connectivity change broadcast
             startListening();
-            cm.setMobileDataEnabled(false);
+            ts.setDataEnabled(false);
         }
 
         @Override
@@ -1018,12 +1009,11 @@ public class SamsungExynos3RIL extends RIL implements CommandsInterface {
             switch(msg.what) {
             //networktype was set, now we can enable the dataconnection again
             case MESSAGE_SET_PREFERRED_NETWORK_TYPE:
-                ConnectivityManager cm =
-                    (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-
+                TelephonyManager ts =
+                    (TelephonyManager)mContext.getSystemService(Context.TELEPHONY_SERVICE);
                 Rlog.d(RILJ_LOG_TAG, "preferred NetworkType set upping Mobile Dataconnection");
 
-                cm.setMobileDataEnabled(true);
+                ts.setDataEnabled(true);
                 //everything done now call back that we have set the networktype
                 AsyncResult.forMessage(mNetworktypeResponse, null, null);
                 mNetworktypeResponse.sendToTarget();
